7d6f52873d104c767bc445095ab20603a03a1620
tayab-pabel <tayabpabel@yahoo.com>
2020-11-19 02:53:05 +0600

    Contact Section Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Contact from './Components/Contact/Contact';
 import Header from './Components/Header/Header/Header';
+import Service from './Components/Service/Service';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Service />
+      <Contact />
     </div>
   );
 }
