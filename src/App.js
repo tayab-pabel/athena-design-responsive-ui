f7496bf1ee862e084f38dacefd3b07d863b1a6bf
ImPias <kazipias123@gmail.com>
2020-11-20 01:56:43 +0600

    Subscribe Section Done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from './Components/Contact/Contact';
 import Header from './Components/Header/Header/Header';
 import Service from './Components/Service/Service';
 import Pricing from './Components/Pricing/Pricing';
+import Subscribe from './Components/Subscribe/Subscribe';
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <Header />
       <Service />
       <Contact />
-      <Achievement/>
-      <Pricing/>
+      <Achievement />
+      <Pricing />
+      <Subscribe />
     </div>
   );
 }
